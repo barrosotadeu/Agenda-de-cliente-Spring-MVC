3ac474e23b4543baec180a513d287c8531f971bf
Tadeu <barrosotadeu3105@gmail.com>
2022-12-30 18:35:24 -0300

    Função cadastrar do CRUD completa

--- src/main/java/br/com/tadeu/agenda/de/clientes/spring/controller/ClienteController.java
+++ src/main/java/br/com/tadeu/agenda/de/clientes/spring/controller/ClienteController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.tadeu.agenda.de.clientes.spring.dto.RequisicaoCliente;
 import br.com.tadeu.agenda.de.clientes.spring.model.Cliente;
 import br.com.tadeu.agenda.de.clientes.spring.repository.ClienteRepository;
+import jakarta.validation.Valid;
 
 @Controller
 public class ClienteController {
@@ -24,6 +28,33 @@ public class ClienteController {
 		mv.addObject("clientes", clientes);
 		System.out.println(clientes);
 		
+		return mv;
+	}
+	
+	@GetMapping("/clientes/novo")
+	public String novo(RequisicaoCliente requisicao) {
+		
+		return "clientes/novo";
+	}
+	
+	@PostMapping("/clientes")
+	public ModelAndView cadastra(@Valid RequisicaoCliente requisicao, BindingResult bindingResult) {
+		System.out.println(requisicao);
+		
+		if(bindingResult.hasErrors()) {
+			
+			System.out.println("**********TEM ERRO*********");
+			return new ModelAndView("clientes/novo");
+		}
+		ModelAndView mv = new ModelAndView("clientes/lista");
+		
+		Cliente cliente = new Cliente();
+		requisicao.transformaRequisicaoEmCliente(cliente);
+		
+		this.clienteRepository.save(cliente);
+		
+		List<Cliente> clientes = this.clienteRepository.findAll();
+		mv.addObject("clientes", clientes);
 		return mv;
 	}
 }
---
+++ src/main/java/br/com/tadeu/agenda/de/clientes/spring/dto/RequisicaoCliente.java
@@ -0,0 +1,56 @@
+package br.com.tadeu.agenda.de.clientes.spring.dto;
+
+import br.com.tadeu.agenda.de.clientes.spring.model.Cliente;
+import jakarta.validation.constraints.NotBlank;
+
+public class RequisicaoCliente {
+	
+	@NotBlank
+	private String nome;
+	@NotBlank
+	private String cpf;
+	@NotBlank
+	private String telefone;
+	@NotBlank
+	private String email;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "Nome: " + nome + ", CPF: " + cpf + ", Telefone: " + telefone + ", Email: " + email;
+	}
+	
+	public void transformaRequisicaoEmCliente(Cliente cliente) {
+		cliente.setNome(nome);
+		cliente.setCpf(cpf);
+		cliente.setTelefone(telefone);
+		cliente.setEmail(email);
+	
+	}
+	
+	
+}
